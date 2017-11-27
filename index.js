a98329c0a4c8865ef878e3b283d8759a53647786
Kolja Lange <mail@klange.org>
2017-11-27 17:19:33 +0100

    feat(template) allow className

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ function template(name, icon) {
     return `import React from 'react';
 import style from '../style';
 
-export default function ${name}() {
+export default function ${name}(props) {
     return (
-        <svg style={style} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <svg style={style} className={props.className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             ${icon}
         </svg>
     );
