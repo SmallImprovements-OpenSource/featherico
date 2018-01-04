95a8d40d3b9e68550a6500b024603f6ff1983da4
Kolja Lange <mail@klange.org>
2018-01-04 17:16:59 +0100

    chore: use github token added by semantic release

--- publishExample.js
+++ publishExample.js
@@ -2,7 +2,7 @@ const { post } = require('axios');
 const { repository } = require('./package.json');
 const commit = process.env.TRAVIS_COMMIT;
 
-post(`https://api.github.com/repos/${repository}/statuses/${commit}?access_token=${process.env.GITHUB_TOKEN}`, {
+post(`https://api.github.com/repos/${repository}/statuses/${commit}?access_token=${process.env.GH_TOKEN}`, {
     state: 'success',
     target_url: `https://storage.googleapis.com/featherico-examples/${commit}.html`,
     description: 'View generated icons',
