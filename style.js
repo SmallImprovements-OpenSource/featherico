1e0d25830b3adf186d5eb9fcbf33730168af4955
Kolja Lange <mail@klange.org>
2018-02-07 11:55:32 +0100

    feat: improve vertical alignment

--- style.js
+++ style.js
@@ -2,6 +2,9 @@ export default {
     display: 'inline-block',
     width: 'var(--fi-size, 1.1em)',
     height: 'var(--fi-size, 1.1em)',
-    marginBottom: 'calc((1em - var(--fi-size, 1.1em)) * 2)',
     strokeWidth: 'var(--fi-stroke, 2px)',
+    marginTop: 'calc((1em - var(--fi-size, 1.1em)) / 2)',
+    marginBottom: 'calc((1em - var(--fi-size, 1.1em)) / 2)',
+    position: 'relative',
+    top: '.14em',
 };
